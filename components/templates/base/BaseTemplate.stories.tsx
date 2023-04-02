346a51bb2e421a23c72a6e870cc6e72595565322
Nathan <nathanhoang123@gmail.com>
2023-04-02 09:33:06 -0400

    refactor: update directory structure

--- components/templates/base/BaseTemplate.stories.tsx
+++ components/templates/base/BaseTemplate.stories.tsx
@@ -1,8 +1,8 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import BaseTemplate, {
   IBaseTemplate,
-} from '../components/templates/base/BaseTemplate';
-import { mockBaseTemplateProps } from '../components/templates/base/BaseTemplate.mocks';
+} from './BaseTemplate';
+import { mockBaseTemplateProps } from './BaseTemplate.mocks';
 
 export default {
   title: 'templates/BaseTemplate',
